5cf31120f274ecdc13951edc622a35847a2ae11f
Leonard König <leonard.r.koenig@googlemail.com>
2016-08-09 21:39:42 +0200

    minor adjustments

--- fancontroller_linux.c
+++ fancontroller_linux.c
@@ -3,9 +3,6 @@
 #include <assert.h>
 #include <errno.h>
 
-#include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 /*
  * You must compile with -O or -O2 or similar.  The functions are  defined  as
  * inline macros, and will not be substituted in without optimization enabled,
@@ -25,29 +22,26 @@
 /**
  * Sets up IOPORTS interface.
  *
- * @return filedescriptor for interface
+ * @return 0 on success
  */
 int ec_intro_sequence();
 
 /**
  * Cleans up IOPORTS.
  *
- * @param filedescriptor to use
- * @return 0 on success, -1 on failure
+ * @return 0 on success
  */
 int ec_outro_sequence();
 
 /**
  * Waits for a bitmask to appear
  *
- * Loops for some time until the given port on the filedescriptor has value
- * if for specified bitmask.
+ * Loops for some time until the given port has value for specified bitmask.
  *
- * @param filedescriptor to use
  * @param port to read from
  * @param bitmask to apply
  * @param value to check. Must be positive.
- * @return 0 when bitmask appears, -1 if it doesn't
+ * @return 0 on success
  */
 int wait_until_bitmask_is_value(unsigned short port, unsigned char bitmask,
 		unsigned char value);
@@ -88,10 +82,7 @@ int main(int argc, char *argv[])
 
 	assert(cmd == MAX || cmd == NORMAL);
 
-	ioperm(0x80, 1, 1); // needed for inb_p()
-	ioperm(0x68, 5, 1);
-
-	if (ec_intro_sequence() < 0) {
+	if (ec_intro_sequence() != 0) {
 		fprintf(stderr, ERRPREF "Unable to perform intro sequence. "
 				"Cleaning up\n");
 		ec_outro_sequence();
@@ -112,6 +103,14 @@ int main(int argc, char *argv[])
 
 int ec_intro_sequence()
 {
+	if (ioperm(0x80, 1, 1) != 0) { /* needed for inb_p() */
+		perror(__func__);
+		return -1;
+	}
+	if (ioperm(0x68, 5, 1) != 0) {
+		perror(__func__);
+	}
+
 	if (wait_until_bitmask_is_value(0x6C, 0x80, 0x00) != 0) {
 		return -1;
 	}
@@ -120,14 +119,15 @@ int ec_intro_sequence()
 	if (wait_until_bitmask_is_value(0x6C, 0x02, 0x00) != 0) {
 		return -1;
 	}
-
 	outb(0x59, 0x6C);
+
 	return 0;
 }
 
 int ec_outro_sequence()
 {
 	inb(0x68);
+
 	if (wait_until_bitmask_is_value(0x6C, 0x02, 0x00) != 0) {
 		return -1;
 	}
@@ -143,12 +143,13 @@ int wait_until_bitmask_is_value(unsigned short port, unsigned char bitmask,
 	assert(value > 0);
 
 	for (int i = 0; i < 10000; i++) {
-		// inb_p sleeps for a short time.
+		/* inb_p sleeps for a short time. */
 		if ((inb_p(port) & bitmask) == value) {
 			return 0;
 		}
 	}
-	fprintf(stderr, ERRPREF "Timeout waiting for mask %x with value %x on port %x\n",
+	fprintf(stderr, ERRPREF "Timeout waiting for mask %x with value %x on"
+			"port %x\n",
 			bitmask, value, port);
 	return -1;
 }
